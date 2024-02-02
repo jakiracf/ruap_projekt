49e897c5f979276504e1cfb4dfe6f3bb1333d9e8
jakiracf <filipj12456@gmail.com>
2024-02-02 16:32:49 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// .NET Framework 4.7.1 or greater must be used
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CallRequestResponseService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvokeRequestResponseService().Wait();
+        }
+
+        static async Task InvokeRequestResponseService()
+        {
+            var handler = new HttpClientHandler()
+            {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                ServerCertificateCustomValidationCallback =
+                        (httpRequestMessage, cert, cetChain, policyErrors) => { return true; }
+            };
+            using (var client = new HttpClient(handler))
+            {
+                // Request data goes here
+                // The example below assumes JSON formatting which may be updated
+                // depending on the format your endpoint expects.
+                // More information can be found here:
+                // https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+                var requestBody = @"{
+                  ""Inputs"": {
+                    ""data"": [
+                      {
+                        ""island"": ""Torgersen"",
+                        ""culmen_length_mm"": ""38.2"",
+                        ""culmen_depth_mm"": ""19"",
+                        ""flipper_length_mm"": ""187"",
+                        ""body_mass_g"": ""3520"",
+                        ""sex"": ""MALE""
+                      }
+                    ]
+                  },
+                  ""GlobalParameters"": {
+                    ""method"": ""predict""
+                  }
+                }";
+
+                client.BaseAddress = new Uri("http://523bf5a5-e167-4319-ab0a-5371a436cd30.germanywestcentral.azurecontainer.io/score");
+
+                var content = new StringContent(requestBody);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                // WARNING: The 'await' statement below can result in a deadlock
+                // if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false)
+                // so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+                HttpResponseMessage response = await client.PostAsync("", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp,
+                    // which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
